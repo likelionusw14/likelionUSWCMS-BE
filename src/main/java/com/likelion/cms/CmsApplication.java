4944e164a4b5a7e8eea7dce6871de629fe1e561a
EC2 Default User <ec2-user@ip-172-31-35-253.ap-northeast-2.compute.internal>
2026-07-20 09:52:06 +0000

    서버 인프라 환경 설정 및 코드 수정

--- src/main/java/com/likelion/cms/CmsApplication.java
+++ src/main/java/com/likelion/cms/CmsApplication.java
@@ -2,8 +2,10 @@ package com.likelion.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.likelion.cms")
 public class CmsApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/likelion/cms/domain/project/controller/ProjectController.java
+++ src/main/java/com/likelion/cms/domain/project/controller/ProjectController.java
@@ -1,14 +1,13 @@
 package com.likelion.cms.domain.project.controller;
-
-import com.likelion.cms.domain.project.service.ProjectService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/projects")
+@RequestMapping("/projects")
 public class ProjectController {
-
-    private final ProjectService projectService;
+    @GetMapping
+    public String getProjects() {
+        return "SUCCESS";
+    }
 }
