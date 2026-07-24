149b953383882924d34064fe43851d2e01a04456
Samoyed-02 <gus02031365@gmail.com>
2026-07-25 02:22:36 +0900

    feat: AppUserRepository에 cohort별 MEMBER 조회 쿼리 추가

--- src/main/java/com/likelion/cms/domain/user/repository/AppUserRepository.java
+++ src/main/java/com/likelion/cms/domain/user/repository/AppUserRepository.java
@@ -1,7 +1,18 @@
 package com.likelion.cms.domain.user.repository;
 
-import com.likelion.cms.domain.user.entity.AppUser;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.likelion.cms.domain.user.entity.AccountStatus;
+import com.likelion.cms.domain.user.entity.AppUser;
+import com.likelion.cms.domain.user.entity.SystemRole;
+
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    List<AppUser> findAllByCohort_CohortIdAndSystemRoleAndAccountStatus(
+            Long cohortId,
+            SystemRole systemRole,
+            AccountStatus accountStatus
+    );
 }
